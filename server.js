f5f48ad0a36b5640a0b3e43f51cce4c38c1361bf
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2015-12-07 12:06:19 -0800

    add form submission components to server

--- server.js
+++ server.js
@@ -2,13 +2,27 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.use(function(req, res, next){
+ res.header("Access-Control-Allow-Origin", "*")
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+ res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+ next()
+});
+
 app.use(bodyParser.json());
 
-var db = {
+app.listen(3000, function() {
+	console.log('test server started');
+});
 
+var db = {
+	users: []
 };
 
-//listener
-app.listen(3000, function(req,res) {
-	console.log('server started');
+app.post('/users', function(req, res){
+	var users = req.body;
+	db.users.push(users);
+	console.log('success');
+	res.json({msg: 'posted'});
+	console.log(db);
 });
