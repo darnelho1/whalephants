aee74c4a3a82be4a5286475fd2c91d175d86fac4
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2015-12-15 17:52:19 -0800

    add keycode submit, close #65

--- thanksPage/thanks.js
+++ thanksPage/thanks.js
@@ -1,6 +1,14 @@
-// button handler
+// Button handler
 $('#sigButton').on('click', function() {
 	$('#thanksDiv').append('<p class="signature">'+$('#thanksIn').val()+'</p>');
 	$('input').remove();
 	$('#sigButton').remove();
 });
+
+// Enter keycode submit
+$('#thanksIn').keypress(function(event){
+	if(event.keyCode === 13) {
+		event.preventDefault();
+		$('#sigButton').click();
+	}
+})
